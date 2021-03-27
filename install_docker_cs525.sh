8d4b0908786e301bb64c743d7b209e1562c5033a
Tian <beitong2@sp21-cs525-g20-04.cs.illinois.edu>
2021-03-27 16:29:39 -0500

    optimized

--- install_docker_cs525.sh
+++ install_docker_cs525.sh
@@ -9,11 +9,9 @@ sudo yum-config-manager \
 sudo yum install -y docker-ce docker-ce-cli containerd.io
 sudo systemctl enable docker
 sudo systemctl start docker
-sudo groupadd docker
 sudo usermod -aG docker $USER
-newgrp docker
-
-curl https://bootstrap.pypa.io/pip/2.7/get-pip.py -o get-pip.py
-python get-pip.py
-pip install docker-py
+sudo curl https://bootstrap.pypa.io/pip/2.7/get-pip.py -o get-pip.py
+sudo python get-pip.py
+sudo pip install docker-py
 sudo yum install -y nc
+newgrp docker 
